1d0e45fd70033df749e2a7ad4e316ef7d7badd50
Azaar-se <azaar19@yahoo.com>
2023-05-02 19:25:21 -0700

    More SLL

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - This finds the loop in a linked list
+ * @srch: This indicates the linked list to search for
+ *
+ * Return: The address of the node where the loop starts, or NULL
+ */
+listint_t *find_listint_loop(listint_t *srch)
+{
+	listint_t *slow = srch;
+	listint_t *fast = srch;
+
+	if (!srch)
+		return (NULL);
+
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = srch;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+
+	return (NULL);
+}
