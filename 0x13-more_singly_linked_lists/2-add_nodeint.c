4903aa7868a9ef03c45dcf06f7d2c6c4c670105d
Azaar-se <azaar19@yahoo.com>
2023-05-02 17:59:05 -0700

    More SLL

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,12 +1,13 @@
-include "lists.h"
+#include "lists.h"
+
 /**
  * add_nodeint - This adds a new node at the beginning of a linked list
- * @fnode: The pointer to the first node in the list
- * @q: The data to insert in that new node
+ * @head: The pointer to the first node in the list
+ * @n: The data to insert in that new node
  *
  * Return: The pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint(listint_t **fnode, const int q)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
@@ -14,9 +15,9 @@ listint_t *add_nodeint(listint_t **fnode, const int q)
 	if (!new)
 		return (NULL);
 
-	new->q = q;
-	new->next = *fnode;
-	*fnode = new;
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
 	return (new);
 }
